412fa6aa72d7b3039148dce13b9ae66a7c49ca8a
eleven <eleven.image@gmail.com>
2017-10-21 23:54:19 +0800

    add dynamic

---
+++ src/store/dynamic.js
@@ -0,0 +1,14 @@
+/*
+* @Author: eleven
+* @Date:   2017-10-21 23:21:38
+* @Last Modified by:   eleven
+* @Last Modified time: 2017-10-21 23:53:57
+*/
+
+import React, { Component } from 'react';
+
+class Async extends Component {
+
+}
+
+export default Async
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+/*
+* @Author: eleven
+* @Date:   2017-10-21 23:14:41
+* @Last Modified by:   eleven
+* @Last Modified time: 2017-10-21 23:54:00
+*/
+
