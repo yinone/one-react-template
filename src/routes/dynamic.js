c3928055952b777163147fbd0c4a17566744d653
eleven <eleven.image@gmail.com>
2017-10-23 23:38:05 +0800

    update

---
+++ src/routes/dynamic.js
@@ -0,0 +1,40 @@
+/*
+* @Author: eleven
+* @Date:   2017-10-21 23:21:38
+* @Last Modified by:   eleven
+* @Last Modified time: 2017-10-22 01:23:52
+*/
+
+// code from git@github.com:didierfranc/react-code-splitting.git
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class Async extends React.Component {
+  componentWillMount = () => {
+    this.cancelUpdate = false
+    this.props.load.then((c) => { 
+      this.C = c
+      if (!this.cancelUpdate) {
+        this.forceUpdate()
+      }
+    })
+  }
+
+  componentWillUnmount = () => {
+    this.cancelUpdate = true
+  }
+
+  render = () => {
+    const { componentProps } = this.props
+    return this.C
+      ? this.C.default
+        ? <this.C.default {...componentProps} />
+        : <this.C {...componentProps} />
+      : null
+  }
+}
+
+Async.propTypes = {
+  load: PropTypes.instanceOf(Promise).isRequired,
+}
