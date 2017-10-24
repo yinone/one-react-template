c524ec6467476c339d1f3f221798b802c1cea9d2
eleven <eleven.image@gmail.com>
2017-10-24 23:25:51 +0800

    update router

--- src/routes/dynamic.js
+++ src/routes/dynamic.js
@@ -2,7 +2,7 @@
 * @Author: eleven
 * @Date:   2017-10-21 23:21:38
 * @Last Modified by:   eleven
-* @Last Modified time: 2017-10-22 01:23:52
+* @Last Modified time: 2017-10-24 20:40:22
 */
 
 // code from git@github.com:didierfranc/react-code-splitting.git
@@ -11,6 +11,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default class Async extends React.Component {
+  static propTypes = {
+    load: PropTypes.instanceOf(Promise).isRequired
+  } 
   componentWillMount = () => {
     this.cancelUpdate = false
     this.props.load.then((c) => { 
@@ -33,8 +36,4 @@ export default class Async extends React.Component {
         : <this.C {...componentProps} />
       : null
   }
-}
-
-Async.propTypes = {
-  load: PropTypes.instanceOf(Promise).isRequired,
 }
